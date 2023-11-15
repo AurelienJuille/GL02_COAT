8b8f00aa73df3c3b700b8cca6e7f32db80930034
Alexis <alexis.heuschling@utt.fr>
2023-11-15 15:52:19 +0100

    Add all Red levels

--- takeOffTools/engine.js
+++ takeOffTools/engine.js
@@ -2,6 +2,7 @@ var Engine = {
 	check: function(){
 	console.log("Engine Ready !")
         console.log("Blue level checked".blue)
+	console.log("Red level checked".red)
 
 	return true
 	}
--- takeOffTools/radio.js
+++ takeOffTools/radio.js
@@ -3,6 +3,7 @@ var Radio = {
 	check: function(){
 	console.log("Radio Ready !")
         console.log("Blue level checked".blue)
+	console.log("Red level checked".red)
 
 	return true
 	}
