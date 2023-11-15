8ba31f00726598534395e98b926a15924c8720fb
Alexis <alexis.heuschling@utt.fr>
2023-11-15 16:53:56 +0100

    Complétion couleurs

--- takeOffTools/satellite1.js
+++ takeOffTools/satellite1.js
@@ -1,7 +1,10 @@
 var Satellite1 = {
   check: function(){
     console.log("Satellite Ready !")
+    console.log("Blue level checked".blue)
+    console.log("Magenta level checked".magenta)
     console.log("Red level checked".red)
+
     return true
   }
 }
