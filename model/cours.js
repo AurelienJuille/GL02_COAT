178fbe5d1a6151ac795adeb9c6ba7c1296fe8557
Tim843 <118382556+Tim843@users.noreply.github.com>
2023-11-29 12:56:54 +0100

    Add files via upload

---
+++ model/cours.js
@@ -0,0 +1,11 @@
+const Console = require("console");
+var Cours = function(uv, creneau){
+    this.uv = uv;
+    this.creneaux = [].concat(creneau);
+};
+
+Cours.prototype.addCreneau = function (creneau) {
+    this.creneaux.push(creneau);
+}
+
+module.exports = Cours;
---
+++ model/creneau.js
@@ -0,0 +1,12 @@
+var Creneau = function (nomUe, type, capacitaire, jour, heureDebut, heureFin, index, salle) {
+    this.nomUe = nomUe;
+    this.type = type;
+    this.capacitaire = capacitaire;
+    this.jour = jour;
+    this.heureDebut = heureDebut;
+    this.heureFin = heureFin;
+    this.index = index;
+    this.salle = salle;
+};
+
+module.exports = Creneau;
