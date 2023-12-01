b672bfaee6bf6e462363c17aa6f98106ce57e8e4
Tim843 <118382556+Tim843@users.noreply.github.com>
2023-12-01 23:25:35 +0100

    ajout des require dans index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const cli = require("@caporal/core").default;
 
+const capacite = require("./commands/sallecours")
+const classementsalles = require("./commands/classementsalles")
+
+cli
+					.version('parser-cli')
+					.version('0.07')
+					sallecours
+					classementsalles
+cli.run(process.argv.slice(2));
