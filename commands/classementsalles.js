2ea4f8e2e81a8d7b1cedd6fba65930a3ef52908a
Tim843 <118382556+Tim843@users.noreply.github.com>
2023-12-02 13:48:19 +0100

    test classementsalles.js

--- commands/classementsalles.js
+++ commands/classementsalles.js
@@ -3,4 +3,24 @@ const cli = require("@caporal/core").default;
 module.exports = cli
 	// Affiche un classement des salles ordonné selon leur capacité
 	.command('classementsalles', 'Affiche un classement des salles ordonné selon leur capacité.')
-	.action(({logger}) => { }
+	.action(({logger}) => {
+	let parserResult = lectureDonnees(cheminDonnees);
+
+	if(parserResult.errorCount > 0){
+		logger.info("Le fichier .cru contient une erreur".red);
+		return;
+	}
+	
+	if (parserResult.salles.size === 0){
+		logger.info("Le fichier ne contient pas de salles.".red);
+		return;
+	}
+
+	Object.keys(parserResult.salles).map(key => ({
+			salle: key,
+			capacite: parserResult.salles[key]
+		})).sort((a, b) => b.capacite- a.capacite)
+		.forEach((salle, index) => {
+			console.log(`${index+1}. ${salle.salle} (${salle.capacite})`);
+		});
+})
