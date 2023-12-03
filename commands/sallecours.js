ff7b3be8924a8269ce1b88a803d5c2f895290de9
Tim843 <118382556+Tim843@users.noreply.github.com>
2023-12-03 20:14:49 +0100

    test sallecours.js

--- commands/sallecours.js
+++ commands/sallecours.js
@@ -1,9 +1,32 @@
 const cli = require("@caporal/core").default;
-
+const { lectureDonnees, cheminDonnees } = require("../fonctions.js");
 const colors = require('colors');
 
 module.exports = cli
     // Récupérer les salles utilisées par une UE
 	.command('sallescours', "Permet d'afficher quelles sont les salles utilisées par une UE")
 	.argument('<cours>', 'Le cours')
-	//.action(({args, logger}) => { }
+	.action(({args, logger}) => {
+		let parserResult = lectureDonnees(cheminDonnees);
+
+		if (parserResult.errorCount > 0) {
+			logger.info("Le fichier .cru contient une erreur".red);
+			return;
+		}
+
+		if (parserResult.listeCreneaux.filter(c => c.nomUe === args.cours).length === 0) {
+			logger.info("Le cours n'existe pas.".red);
+			return;
+		}
+
+		console.log(
+			"Salles occupées par l'UE",
+			args.cours,
+			":", 
+			[...new Set(
+				parserResult.listeCreneaux
+					.filter(creneau => creneau.nomUe === args.cours)
+					.map(creneau => creneau.salle)
+			)]
+		);
+	})
