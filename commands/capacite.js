2ab6e0b3b01570aadf4b188f1465419e4bdc02a7
Tim843 <118382556+Tim843@users.noreply.github.com>
2023-12-05 22:13:53 +0100

    ajout de la commande capacite.js

--- commands/capacite.js
+++ commands/capacite.js
@@ -6,4 +6,19 @@ module.exports = cli
 	//Capacité max d'une salle parmi tous les créneaux
 	.command('capacite', "Affiche quelle capacité maximale une salle peut avoir.")
 	.argument('<nomSalle>', 'Nom de la salle.')
-	//.action(({ args, logger }) => {}
+	.action(({ args, logger }) => {
+
+		let parserResult = lectureDonnees(cheminDonnees);
+
+		if (parserResult.errorCount > 0) {
+			logger.info("Le fichier .cru contient une erreur".red);
+			return;
+		}
+
+		if (!parserResult.salles[args.nomSalle]) {
+			logger.info("La salle demandé n'existe pas dans la base de donnée.".red)
+		} else {
+			logger.info("La salle " + args.nomSalle + " peut acceuillir au maximum : " + parserResult.salles[args.nomSalle] + " personnes.");
+		}
+		
+	})
