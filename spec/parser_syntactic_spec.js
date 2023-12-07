f4280098b5a2e5ab3e3973fff26b8bfd0a49407f
Tim843 <118382556+Tim843@users.noreply.github.com>
2023-12-07 14:51:36 +0100

    ajout de plusieurs syntactic test

--- spec/parser_syntactic_spec.js
+++ spec/parser_syntactic_spec.js
@@ -19,6 +19,55 @@ describe("Program Syntactic testing of Parser", function(){
 		
 	});
 
+	it("can read a type from a simulated input", function(){
+		
+		let input = ["C1"];
+		expect(this.analyzer.type(input)).toBe("C1");
+		
+	});
+
+	it("can read a capacitaire from a simulated input", function(){
+		
+		let input = [24];
+		expect(this.analyzer.capacitaire(input)).toBe(24);
+		
+	});
+
+	it("can read a jour from a simulated input", function(){
+		
+		let input = ["J"];
+		expect(this.analyzer.jour(input)).toBe("J");
+		
+	});
+
+	it("can read a heureDebut from a simulated input", function(){
+		
+		let input = ["10:00"];
+		expect(this.analyzer.heureDebut(input)).toBe("10:00");
+		
+	});
+
+	it("can read a heureFin from a simulated input", function(){
+		
+		let input = ["12:00"];
+		expect(this.analyzer.heureFin(input)).toBe("12:00");
+		
+	});
+
+	it("can read an index from a simulated input", function(){
+		
+		let input = ["F1"];
+		expect(this.analyzer.index(input)).toBe("F1");
+		
+	});
+
+	it("can read a salle from a simulated input", function(){
+		
+		let input = ["P202"];
+		expect(this.analyzer.salle(input)).toBe("P202");
+		
+	});
+
 
 	/*it("can read a lat lng coordinate from a simulated input", function(){
 		
