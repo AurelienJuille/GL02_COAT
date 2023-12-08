62c3a7603801870cf045d2eda33fc8991bca3542
Tim843 <118382556+Tim843@users.noreply.github.com>
2023-12-08 18:45:11 +0100

    maj parser_syntactic_spec.js

--- spec/parser_syntactic_spec.js
+++ spec/parser_syntactic_spec.js
@@ -68,28 +68,6 @@ describe("Program Syntactic testing of Parser", function(){
 		expect(this.analyzer.salle(input)).toBe("P202");
 		
 	});
-
-
-	/*it("can read a lat lng coordinate from a simulated input", function(){
-		
-		let input = ["latlng", "48.866205;2.399279"];
-		expect(this.analyzer.latlng(input)).toEqual({ lat: "48.866205" , lng: "2.399279" });
-		
-		let inputNeg = ["latlng", "-48.866205;2.399279"];
-		expect(this.analyzer.latlng(inputNeg)).toEqual({ lat: "-48.866205" , lng: "2.399279" });
-		
-	});	
-	
-	it("can read several rankings for a POI from a simulated input", function(){
-		
-		let input = ["note", "1", "note", "7"];
-		
-		this.analyzer.note(input, this.pEmptyRating);
-
-		expect(this.pEmptyRating.ratings[0]).toBe("1");
-		expect(this.pEmptyRating.ratings[1]).toBeUndefined();
-		
-	});	*/
 	
 	/*it("can parse an entire Cours from a simulated input", function(){
 		
